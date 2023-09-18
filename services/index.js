384aafd54b4925b8ab317138d12d9db39b4faa31
Ifeanyi Osi-Okeke <ifeanyi.osi.okeke@gmail.com>
2023-09-18 17:00:39 +0100

    update

---
+++ services/index.js
@@ -0,0 +1,40 @@
+import { graphql } from "graphql";
+import { request, gql } from "graphql-request";
+
+const graphqlAPI = process.env.NEXT_PUBLIC_BLOG_ENDPOINT;
+
+export const getPosts = async () => {
+  const query = gql`
+    query Assets {
+      postsConnection {
+        edges {
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.postsConnection.edges;
+};
