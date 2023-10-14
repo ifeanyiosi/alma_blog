6b3ead7e2affaa4fbe34814140cc8b6c14fb7f80
Ifeanyi Osi-Okeke <ifeanyi.osi.okeke@gmail.com>
2023-10-14 03:47:18 +0100

    update

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import moment from "moment";
 import Image from "next/image";
 import Link from "next/link";
+import { BsFillCalendarCheckFill } from "react-icons/bs";
 
 const PostCard = ({ post }) => {
   console.log(post);
@@ -19,13 +20,42 @@ const PostCard = ({ post }) => {
       </div>
 
       <h1 className="transition duration-700 text-center mb-8 cursor-pointer">
-        <Link href={`/post/ ${post.slug}`}>
+        <Link className="font-bold text-[25px]" href={`/post/ ${post.slug}`}>
           {post.title}
         </Link>
       </h1>
 
-      <div>
-        div
+      <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
+        <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">
+          <Image
+            alt={post.author.name}
+            height={30}
+            width={30}
+            className="align-middle rounded-full"
+            src={post.author.photo.url}
+          />
+
+          <p className="inine align-middle text-gray-700 ml-2 text-lg">
+            {post.author.name}
+          </p>
+        </div>
+        <div className="font-medium flex items-center justify-center gap-x-2 text-gray-700">
+          <BsFillCalendarCheckFill className="text-rose-500" />
+          <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
+        </div>
+      </div>
+
+      <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">
+        {" "}
+        {post.excerpt}{" "}
+      </p>
+
+      <div className="text-center">
+        <Link href={`/post/${post.slug}`}>
+          <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
+            Continue Reading
+          </span>
+        </Link>
       </div>
     </div>
   );
